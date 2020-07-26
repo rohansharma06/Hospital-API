7a027ae7bad1f81057f247fbdba10ccc958b4c0c
rohansharma06 <rohansharma251998@gmail.com>
2020-07-26 16:11:50 +0530

    setting up chai & mocha

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express');
+const express = require("express");
 const port = 8000;
 const app = express();
 
-const db = require('./config/mongoose');
+if (process.env.NODE_ENV !== "test") {
+  const db = require("./config/mongoose");
+}
 
 //---- require passport ans jwt strategy for authentication
-const passport = require('passport');
-const passportJWT = require('./config/passport-jwt-strategy');
+const passport = require("passport");
+const passportJWT = require("./config/passport-jwt-strategy");
 
 //---- to fetch data from url
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 //---- tell app to use passport
 app.use(passport.initialize());
 
 //--- use express router
-    app.use('/', require('./routes'));
-    
+app.use("/", require("./routes"));
+
 //---- starting server
-app.listen(port, function(err){
-    if(err){
-        console.log('Error in running the server:',err);
-        return;
-    }
-    console.log('Server is up and runinng at port: ',port );
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server:", err);
+    return;
+  }
+  console.log("Server is up and runinng at port: ", port);
 });
--- models/doctor.js
+++ models/doctor.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //--- make schema
-const doctorSchema = new mongoose.Schema({
+const doctorSchema = new mongoose.Schema(
+  {
     username: {
-        type:String,
-        required:true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type:String,
-        required:true
-    }
-},{
-    timestamps:true
-});
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Doctor = mongoose.model('Doctor', doctorSchema);
+const Doctor = mongoose.model("Doctor", doctorSchema);
 module.exports = Doctor;

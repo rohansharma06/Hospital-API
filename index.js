0576cf0358c0e55f0ceb0c4a199a30e87a3391f3
rohansharma06 <rohansharma251998@gmail.com>
2020-07-27 07:36:19 +0530

    config server to run test

--- index.js
+++ index.js
@@ -2,16 +2,28 @@ const express = require("express");
 const port = 8000;
 const app = express();
 
-if (process.env.NODE_ENV !== "test") {
-  const db = require("./config/mongoose");
-}
+const db = require("./config/mongoose");
+
+const bodyParser = require("body-parser");
+const config = require("config");
+const morgan = require("morgan");
 
 //---- require passport ans jwt strategy for authentication
 const passport = require("passport");
 const passportJWT = require("./config/passport-jwt-strategy");
 
+//don't show the log when it is test
+if (config.util.getEnv("NODE_ENV") !== "test") {
+  //use morgan to log at command line
+  app.use(morgan("combined"));
+}
+
 //---- to fetch data from url
-app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/json" }));
 
 //---- tell app to use passport
 app.use(passport.initialize());
@@ -27,3 +39,5 @@ app.listen(port, function (err) {
   }
   console.log("Server is up and runinng at port: ", port);
 });
+
+module.exports = app;

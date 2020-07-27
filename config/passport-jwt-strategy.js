21fcf514727e525a8f8c565d43731046c92b52cc
rohansharma06 <rohansharma251998@gmail.com>
2020-07-27 09:16:03 +0530

    update

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,26 +1,30 @@
-const passport=require('passport');
-const JWTStrategy=require('passport-jwt').Strategy;
-const ExtractJWT=require('passport-jwt').ExtractJwt;
+const passport = require("passport");
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
 
-const Doctor=require('../models/doctor');
+const Doctor = require("../models/doctor");
+const { request } = require("chai");
 
 let opts = {
-    jwtFromRequest : ExtractJWT.fromAuthHeaderAsBearerToken(),
-    secretOrKey : "hospitalapi"
-}
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: "hospitalapi",
+};
 
-passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
+passport.use(
+  new JWTStrategy(opts, function (jwtPayLoad, done) {
+    Doctor.findById(jwtPayLoad._id, function (err, doctor) {
+      if (err) {
+        console.log("Error in finding Doctor!");
+        return;
+      }
 
-    Doctor.findById(jwtPayLoad._id, function(err,doctor){
-        if(err){console.log('Error in finding Doctor!');return;}
-
-        if(doctor){
-            return done(null,doctor);
-        }
-        else{
-            return done(null,false);
-        }
-    })
-}))
+      if (doctor) {
+        return done(null, doctor);
+      } else {
+        return done(null, false);
+      }
+    });
+  })
+);
 
 module.exports = passport;
